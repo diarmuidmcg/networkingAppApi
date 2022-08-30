0c32a346851ec253254fa4c5ae275ad29b20fe4d
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2022-08-30 16:26:08 -0400

    added boilerplate to support image upload on events

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -11,30 +11,30 @@ const validateDate = (date) => {
   // return date.match(
   //   /^\d{4}\/\d{1,2}\/\d{1,2}$/,
   // );
-  if(!date.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/))
+  if (
+    !date.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/)
+  )
     return false;
-  var parts = date.split("/");
+  var parts = date.split('/');
   var day = parseInt(parts[0], 10);
   var month = parseInt(parts[1], 10);
   var year = parseInt(parts[2], 10);
 
   // Check the ranges of month and year
-  if(year < 1000 || year > 3000 || month == 0 || month > 12)
-    return false;
+  if (year < 1000 || year > 3000 || month == 0 || month > 12) return false;
 
-  var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+  var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   // Adjust for leap years
-  if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
     monthLength[1] = 29;
 
   // Check the range of the day
-  return day > 0 && day <= monthLength[month - 1];  
+  return day > 0 && day <= monthLength[month - 1];
   return true;
 };
 const validateTime = (time) => {
-  if(!time.match(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/))
-    return false
+  if (!time.match(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/)) return false;
   return true;
 };
 @Injectable()
@@ -69,22 +69,15 @@ export class EventsService {
     return response.status(200).json(event);
   }
 
-  public async createEvent(request, response): Promise<Events[]> {
-    const {
-      title,
-      location,
-      date,
-      time,
-      price,
-      description,  
-    } = request.body;
+  public async createEvent(files, request, response): Promise<Events[]> {
+    const { title, location, date, time, price, description } = request.body;
 
     // type check all NOT Nullable
     let mustInclude = [];
     if (!title) mustInclude.push('title');
     if (!location) mustInclude.push('location');
     if (!date) mustInclude.push('date');
-    
+
     // type check date and time
     if (!validateDate(date)) mustInclude.push('date must be proper format');
     if (!validateTime(time)) mustInclude.push('time must be proper format');
@@ -101,16 +94,14 @@ export class EventsService {
     newEvent.date = date !== null ? date : '';
     newEvent.time = time !== null ? time : '';
     newEvent.price = price !== null ? price : '';
-    newEvent.description =
-    description !== null ? description : '';
-    
+    newEvent.description = description !== null ? description : '';
 
     globalThis.Logger.log({ level: 'info', message: 'New Event' });
     globalThis.Logger.log({ level: 'info', message: JSON.stringify(newEvent) });
 
     // try {
-      const data = await this.eventsRepository.save(newEvent);
-      globalThis.Logger.log({
+    const data = await this.eventsRepository.save(newEvent);
+    globalThis.Logger.log({
       level: 'info',
       message: 'New Event Response ' + JSON.stringify(data),
     });
@@ -122,41 +113,30 @@ export class EventsService {
     //   });
     //   return response.status(400).json({ error: e });
     // // }
-    
   }
 
-  public async updateEvent(request, response): Promise<Events[]> {
-    const {
-        title,
-        location,
-        date,
-        time,
-        price,
-        description,
-    } = request.body;
+  public async updateEvent(files, request, response): Promise<Events[]> {
+    const { title, location, date, time, price, description } = request.body;
     const { id } = request.params;
     const updatedEvent = new Events();
-    
+
     // type check all NOT Nullable
     let mustInclude = [];
     if (title != undefined) updatedEvent.title = title;
     if (location != undefined) updatedEvent.location = location;
     if (date != undefined) {
       // type check date
-      if (!validateDate(date))
-        mustInclude.push('date must be proper format');
+      if (!validateDate(date)) mustInclude.push('date must be proper format');
       updatedEvent.date = date;
     }
     if (time != undefined) {
       //type check time
-      if(!validateTime(time))
-        mustInclude.push('time must be a proper format')
+      if (!validateTime(time)) mustInclude.push('time must be a proper format');
       updatedEvent.time = time;
     }
 
     if (price != undefined) updatedEvent.price = price;
     if (description != undefined) updatedEvent.description = description;
-    
 
     if (mustInclude.length > 0)
       return response.status(400).json({
